d1fc8ea30de765ff0271d1a18752c6aaaa8092d8
Alexis-VS-CSharp <alexmart1.alexis@gmail.com>
2023-12-22 04:41:15 +0100

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,3 +1,4 @@
+@echo off
 md temp
 move cartographie.db %cd%\temp
 move journal.db %cd%\temp
@@ -5,3 +6,7 @@ move targetlist.db %cd%\temp
 move config.yml %cd%\temp
 cd %cd%
 git clone https://github.com/Alexis-VS-CSharp/Galaxy-Life-Alliance-Bot.git
+move %cd%\Galaxy-Life-Alliance-Bot\*.* %cd%
+rmdir /s /q Galaxy-Life-Alliance-Bot
+move %cd%\temp\*.* %cd%
+rmdir /s /q temp
