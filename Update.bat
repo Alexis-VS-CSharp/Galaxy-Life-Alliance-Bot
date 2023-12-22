19fa452e460b350a4ac99c2cd6738bc3db2e1ba3
Alexis-VS-CSharp <alexmart1.alexis@gmail.com>
2023-12-22 04:14:30 +0100

    Adding an update executable

---
+++ Update.bat
@@ -0,0 +1,7 @@
+md temp
+move cartographie.db %cd%\temp
+move journal.db %cd%\temp
+move targetlist.db %cd%\temp
+move config.yml %cd%\temp
+cd %cd%
+git clone https://github.com/Alexis-VS-CSharp/Galaxy-Life-Alliance-Bot.git
