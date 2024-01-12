2899e97e81bc814d12f4d9a084bf739e437c8170
Alexis-VS-CSharp <alexmart1.alexis@gmail.com>
2024-01-12 13:05:06 +0100

    Update.bat updated and now works

--- Update.bat
+++ Update.bat
@@ -1,12 +1,8 @@
 @echo off
-md temp
-move cartographie.db %cd%\temp
-move journal.db %cd%\temp
-move targetlist.db %cd%\temp
-move config.yml %cd%\temp
-cd %cd%
 git clone https://github.com/Alexis-VS-CSharp/Galaxy-Life-Alliance-Bot.git
-move %cd%\Galaxy-Life-Alliance-Bot\*.* %cd%
+del .\Galaxy-Life-Alliance-Bot\cartographie.db
+del .\Galaxy-Life-Alliance-Bot\journal.db
+del .\Galaxy-Life-Alliance-Bot\targetlist.db
+del .\Galaxy-Life-Alliance-Bot\config.yml
+move .\Galaxy-Life-Alliance-Bot\* .\
 rmdir /s /q Galaxy-Life-Alliance-Bot
-move %cd%\temp\*.* %cd%
-rmdir /s /q temp
